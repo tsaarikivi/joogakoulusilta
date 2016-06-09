ce19829d7a75234dcbfdeec8f5227ddc4f168d04
tsaarikivi <tero.saarikivi@outlook.com>
2016-06-09 22:27:15 +0300

    initial commit

--- src/dev/app.js
+++ src/dev/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./views/Layout.js";
+import Layout from "./views/Layout";
 
 const app = document.getElementById('app');
 
