0aab27a1ab48f195340644790e0e9e13ca1bce10
tsaarikivi <tero.saarikivi@outlook.com>
2016-06-14 09:40:25 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import firebase from "firebase";
 
 require('./styles/app.scss');
 
 import Layout from "./dev/views/Layout.jsx";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCambUGIYI2HeiwG6ril_5dEVEC8Av6oGI",
+  authDomain: "testproject-b9597.firebaseapp.com",
+  databaseURL: "https://testproject-b9597.firebaseio.com",
+  storageBucket: "testproject-b9597.appspot.com",
+};
+firebase.initializeApp(config);
+
+firebase.database().ref('items/').push({
+  name: "tero",
+  size: 11
+})
 
 const app = document.getElementById('app');
 
