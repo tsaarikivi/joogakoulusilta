2c9378c69b03fa472b6f931a13e100622bed073e
Tuomo <tuomo.saarikivi@outlook.com>
2016-06-18 09:15:43 -0700

    braintree updated

--- src/dev/views/Checkout.jsx
+++ src/dev/views/Checkout.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import Jquery from "jquery"
+
 var Braintree = require("braintree-web");
-import ClientToken from "../components/shop/ClientToken.jsx";
 import DropIn from "../components/shop/BraintreeDropIn.jsx";
 
 export default class Checkout extends React.Component {
 
   constructor(){
     super();
-    this.TokenFactory = new ClientToken;
-
+    this.getToken();
   }
 
+  getToken() {
+    console.log("requesting client token");
+    var that = this;
+    try {
+      Jquery.ajax({
+           async: false,
+           type: 'GET',
+           url: 'http://localhost:3000',
+           success: function(result) {
+             that.token = result;
+             console.log("Token: " + result);
+           }
+      });
+      } catch(error) {
+        // Handle error
+        console.log("TOKEN_ERROR:");
+        console.error(error);
+      }
+  }
 
   onReady() {
       console.log('Drop-In ready');
@@ -28,13 +47,14 @@ export default class Checkout extends React.Component {
   }
 
   render() {
+    console.log('token-is: ' + this.token);
     return (
           <div>
             <p>Testaa numerolla: <code>4111 1111 1111 111</code> Päivämärä voi olla mikä vaan.</p>
             <form action='/transactions' method='POST'>
                 <DropIn
                     braintree={Braintree}
-                    clientToken={this.TokenFactory.getToken()}
+                    clientToken={this.token}
                     onReady={this.onReady}
                     onError={this.onError}
                     onPaymentMethodReceived={this.onPaymentMethodReceived}
