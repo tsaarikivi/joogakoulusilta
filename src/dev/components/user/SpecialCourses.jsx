a04b3f6122890e535004d784bcc2c94c4d5f4687
tsaarikivi <tero.saarikivi@outlook.com>
2016-06-18 12:05:26 +0300

    random

--- src/dev/components/user/SpecialCourses.jsx
+++ src/dev/components/user/SpecialCourses.jsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { Link } from "react-router"
-import firebase from "firebase";
 
 export default class SpecialCourses extends React.Component {
-  getItems() {
+  constructor() {
+    super();
+    this.state = {courses: [{title: "moi"}]}
+  }
+
+  componentWillMount() {
     var database = this.props.database
-    var specialCoursesRef = database.ref('/specialCourses/');
-    return specialCoursesRef;
+    var that = this;
+    var specialCoursesRef = database.ref('/specialCourses');
+    specialCoursesRef.once("value", function(snapshot){
+      var courses = [];
+      snapshot.forEach(function(data){
+        var course = {
+          title: data.val().title,
+          time: data.val().time,
+          description: data.val().description
+        }
+        courses.push(course);
+      })
+      that.setState({courses: courses});
+    });
+  }
+
+  componentWillUnmount() {
+    this.firebaseRef.off();
   }
 
   render() {
-    console.log(this.getItems())
     return (
       <div class="container">
         <h2>Tulevia erityiskursseja</h2>
         <div class="items-body">
           <span class="item-card">
-            <h3>Joogakurssi 1</h3>
+            <h3>{this.state.courses[0].title}</h3>
             <p>1.8 klo 12:00</p>
             <button class="btn-small">lisätietoja</button>
           </span>
