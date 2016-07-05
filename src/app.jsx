14185549e172760afcc0ca389bcb65fdcd7d40db
Tuomo <tuomo.saarikivi@outlook.com>
2016-07-05 00:37:37 -0700

    removed server and moved firebase initialization from index.html to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -11,6 +11,19 @@ require('./styles/app.scss');
 
 var store = createStore(reducer, applyMiddleware(thunk));
 
+//================================================================
+//Initialize firebase as a globally accessible property
+//=============
+global.firebase = require('firebase')
+var firebaseConfig = {
+  apiKey: "AIzaSyCq544Yq7EEY-5spIe1oFCe8gkOzRkS5ak",
+  authDomain: "joogakoulusilta-projekti.firebaseapp.com",
+  databaseURL: "https://joogakoulusilta-projekti.firebaseio.com",
+  storageBucket: "joogakoulusilta-projekti.appspot.com",
+};
+firebase.initializeApp(firebaseConfig);
+//================================================================
+
 const app = document.getElementById('app');
 ReactDOM.render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -63,21 +63,4 @@ module.exports = [
     copyUnmodified: true
     })
   ],
-},
-{
-		// The configuration for the server-side component
-		name: "joogakoulu payment",
-    context: path.join(__dirname, "src"),
-		entry: "./server.js",
-		target: "node",
-		output: {
-			path: __dirname + "/public/",
-			filename: "index.js",
-			publicPath: __dirname + "/public/",
-			libraryTarget: "commonjs2"
-		},
-		externals: /^[a-z\-0-9]+$/,
-		module: {
-			loaders: commonLoaders
-		}
-	}];
+}];
