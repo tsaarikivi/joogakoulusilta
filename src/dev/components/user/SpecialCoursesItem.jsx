d9e69db4a8b6719657bc4647d3e88b802898a2b4
tsaarikivi <tero.saarikivi@outlook.com>
2016-07-21 11:56:25 +0300

    timestring to special courses

--- src/dev/components/user/SpecialCoursesItem.jsx
+++ src/dev/components/user/SpecialCoursesItem.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 
 export default class SpecialCoursesItem extends React.Component {
+
+  constructor(){
+    super();
+    this.startDate = new Date();
+    this.endDate = new Date();
+  }
+
+  getDisplayTime(date,time){
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    date.setTime(date.getTime()+time)
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.getDisplayTime(this.startDate, nextProps.item.start);
+    this.getDisplayTime(this.endDate, nextProps.item.end);
+  }
+
+  componentWillMount(){
+    this.getDisplayTime(this.startDate, this.props.item.start);
+    this.getDisplayTime(this.endDate, this.props.item.end);
+  }
+
   render() {
     return (
       <li className="special-course-item">
         <p>{this.props.item.courseType.name}</p>
         <p>{this.props.item.date}</p>
-        <p>klo {this.props.item.start} - {this.props.item.end}</p>
+        <p>klo {this.startDate.toTimeString().slice(0,5)} - {this.endDate.toTimeString().slice(0,5)}</p>
       </li>
     );
   }
