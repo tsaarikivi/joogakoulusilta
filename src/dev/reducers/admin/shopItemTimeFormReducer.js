ffa91461a48f32b16ad9afd36ca4072fa5a6fea3
tsaarikivi <tero.saarikivi@outlook.com>
2016-07-26 22:22:19 +0300

    add kauppa check

--- src/dev/reducers/admin/shopItemTimeFormReducer.js
+++ src/dev/reducers/admin/shopItemTimeFormReducer.js
@@ -8,9 +8,9 @@ const INITIAL_STATE = {
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case EXPAND_TIME_SHOP_FORM:
-      return Object.assign({}, state, {expanded: true})
+      return Object.assign({}, state, action.payload)
     case MINIMIZE_TIME_SHOP_FORM:
-      return Object.assign({}, state, {expanded: false})
+      return Object.assign({}, state, action.payload)
     default:
       return state
   }
