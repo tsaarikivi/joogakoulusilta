de2a6908c19fdca88633354dfc757897ba253b40
tsaarikivi <tero.saarikivi@outlook.com>
2016-07-27 17:50:54 +0300

    styling

--- src/dev/components/admin/AdminHeader.jsx
+++ src/dev/components/admin/AdminHeader.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router"
 export default class AdminHeader extends React.Component {
   render() {
     return (
-      <div class="container bordered-container centered">
-        <h1>Hallinnointisivu</h1>
-        <Link className="text-link back-btn" to="user">&lt;Takaisin</Link>
+      <div class="container bordered-container">
+        <div className="content-container">
+          <Link className="text-link back-btn" to="user">&lt;Takaisin</Link>
+          <h1>Hallinnointisivu</h1>
+        </div>
       </div>
     );
   }
