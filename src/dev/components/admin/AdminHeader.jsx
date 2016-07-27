004fc1be90abed0f4c333592338931ccd48626e3
tsa <tuomo.saarikivi@outlook.com>
2016-07-27 11:09:06 +0300

    takaisin

--- src/dev/components/admin/AdminHeader.jsx
+++ src/dev/components/admin/AdminHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router"
 
 export default class AdminHeader extends React.Component {
   render() {
     return (
       <div class="container bordered-container centered">
         <h1>Hallinnointisivu</h1>
+        <Link className="text-link back-btn" to="user">&lt;Takaisin</Link>
       </div>
     );
   }
