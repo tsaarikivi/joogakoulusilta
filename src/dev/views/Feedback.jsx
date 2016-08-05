7d9100fd0752d42f9ed54a431dedbd3419dd8aa0
tsaarikivi <tero.saarikivi@outlook.com>
2016-08-05 20:36:25 +0300

    add feedback form working

--- src/dev/views/Feedback.jsx
+++ src/dev/views/Feedback.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { reduxForm } from 'redux-form'
 
-export default class Feedback extends React.Component {
+import { sendFeedback } from '../actions/user.js'
+
+class Feedback extends React.Component {
+  static contextTypes = {
+    router: React.PropTypes.object
+  }
+
+  onSubmit(props){
+    this.props.sendFeedback(props.feedback)
+    this.context.router.push('/user/')
+  }
+
   render() {
+    const { fields: { feedback }, handleSubmit } = this.props
+
     return (
-      <div className="container login-container margin-top">
-        moi
+      <div className="container">
+        <div className="content-container login-container">
+            <form onSubmit={handleSubmit(props => this.onSubmit(props))}>
+                <h5>Lähetä palautetta joogakoululle</h5>
+                <textarea type="text" id="feedback" {...feedback}/>
+                {feedback.touched && feedback.error && <div className="form-error">{feedback.error}</div>}
+                <button type="submit" className="btn-small btn-blue">Lähetä</button>
+            </form>
+        </div>
       </div>
     )
   }
 }
+
+function validate(values) {
+  const errors = {}
+
+  if (!values.feedback) {
+    errors.feedback = "Palautteella tulee olla sisältö."
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'FeedbackForm',
+  fields: ['feedback'],
+  validate
+}, null, { sendFeedback })(Feedback)
