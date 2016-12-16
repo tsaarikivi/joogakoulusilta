4952f226aa09ea70ef3c4398216f573c36fa63ec
tsaarikivi <tero.saarikivi@outlook.com>
2016-12-16 18:18:18 +0200

    change contact info

--- src/dev/components/home/ContactInfo.jsx
+++ src/dev/components/home/ContactInfo.jsx
@@ -8,9 +8,9 @@ export default class ContactInfo extends React.Component {
           <h2 class="contact-heading">Yhteystiedot</h2>
           <p class="contact-info">Joogakoulu Silta</p>
           <p class="contact-info">Tallbergin puistotie 7A, 00200 Helsinki</p>
+          <p class="contact-info">Tuula Heiskanen & Hanna Lampen</p>
           <p class="contact-info">joogakoulusilta@gmail.com</p>
-          <p class="contact-info">Tuula Heiskanen puh. 050 373 8656</p>
-          <p class="contact-info">Hanna Lampen puh. 050 443 3370</p>
+          <p class="contact-info">puh. 050 443 3370</p>
           <div class="mobile-centered">
             <a alt="facebook" className="social-btn" href="https://www.facebook.com/joogakoulusilta/" target="_blank"><img src="./assets/facebook.png" /></a>
             <a alt="instagram" className="social-btn" href="https://www.instagram.com/joogakoulusilta/" target="_blank"><img src="./assets/instagram.png" /></a>
